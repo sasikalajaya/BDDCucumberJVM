3540eda4a0b3cd714600968edf54560c85b2d6da
sasikalajaya <sasikalajaya@gmail.com>
2014-07-21 16:54:34 +0100

    Initial commit

---
+++ src/main/java/bdd/cucumber/jvm/myFeatures.feature
@@ -0,0 +1,11 @@
+Feature: Proof of concept that my framework works
+
+  Scenario: My First Test
+    Given this is my first step
+    When  this is my second step
+    And   this is my final step
+
+  Scenario: My Second Test
+    Given this is my 2-first step
+    When  this is my 2-second step
+    And   this is my 2-final step
---
+++ src/test/java/bdd/cucumber/jvm/MyFeatureTest.java
@@ -0,0 +1,27 @@
+package bdd.cucumber.jvm;
+
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+
+public class MyFeatureTest {
+    @Given("^this is my first step$")
+    public void this_is_my_first_step() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^this is my second step$")
+    public void this_is_my_second_step() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^this is my final step$")
+    public void this_is_my_final_step() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+}
