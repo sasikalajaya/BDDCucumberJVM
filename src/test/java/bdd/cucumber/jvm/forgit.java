e3efab70270952abcf62a634b01656a30eaa3c69
sasikalajaya <sasikalajaya@gmail.com>
2014-07-22 14:58:34 +0100

    third

--- src/test/java/bdd/cucumber/jvm/forgit.java
+++ src/test/java/bdd/cucumber/jvm/forgit.java
@@ -4,4 +4,9 @@ package bdd.cucumber.jvm;
  * Created by Sasikala on 21/07/2014.
  */
 public class forgit {
+
+    public static void get(){
+
+        System.out.println("these are some of the changes for github ppush done in the local(intellij)");
+    }
 }
