82cf5347e0e4d7b394d3d22865badde4a4183f8a
sasikalajaya <sasikalajaya@users.noreply.github.com>
2014-07-22 14:57:47 +0100

    Update forgit.java

--- src/test/java/bdd/cucumber/jvm/forgit.java
+++ src/test/java/bdd/cucumber/jvm/forgit.java
@@ -4,4 +4,10 @@ package bdd.cucumber.jvm;
  * Created by Sasikala on 21/07/2014.
  */
 public class forgit {
+ public static void get()
+ { System.out.println("this is a change done to the same method in the Github cloud");  
+ } 
+
 }
+
+
